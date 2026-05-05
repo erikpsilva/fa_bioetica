65cc6433b46906e8938295b4e73990695fa8972c
Erik Primao <erikpsilva@gmail.com>
2026-05-05 11:31:35 -0300

    salvando projeto inicial

---
+++ admin/includes/assets.php
@@ -0,0 +1,13 @@
+<meta name="viewport" content="width=device-width, user-scalable=no">
+
+<meta name="title" content="Fórum Animal Bioética">
+<meta name="description" content="Fórum Animal Bioética">
+<meta name="keywords" content="">
+<meta name="author" content="Fórum Animal Bioética">
+
+<link rel="icon" href="<?= ADMIN_BASE_URL ?>/images/favicon.png" type="image/x-icon"/>
+
+<?php
+$version = time();
+echo '<link rel="stylesheet" type="text/css" href="' . ADMIN_BASE_URL . '/styles/style.min.css?v' . $version . '">';
+?>
